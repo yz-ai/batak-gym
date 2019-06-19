541269f401d234d97aa89fe14b79e7ab14ad3c6f
kivancguckiran <kivancguckiran@gmail.com>
2019-06-19 21:22:30 +0300

    iniital observation structure

---
+++ observation.py
@@ -0,0 +1,17 @@
+
+observation = {
+	'current_player': 0, # 0, 1, 2, 3, 4
+	'player_observations': [
+		{
+			'hand': [
+				{ 'SPADES': 1 },
+				{ 'HEARTS': 3 },
+				# ...
+			],
+			'legal_moves': [
+				{ 'SPADES': 1 },
+				# ...
+			],
+		},
+	],
+}
