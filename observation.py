90d909d02257e51d9195eb9e838850037fe0e243
kivancguckiran <kivancguckiran@gmail.com>
2019-06-20 01:15:50 +0300

    observation updated

--- observation.py
+++ observation.py
@@ -1,15 +1,25 @@
 
 observation = {
 	'current_player': 0, # 0, 1, 2, 3, 4
+	'trump_broken': False,
+	'expecting_bid': False,
+	'expecting_trump': False,
+	'current_trump': 'SPADES',
 	'player_observations': [
 		{
+			'last_play': { 'type': 'DIAMONDS', 'number': 1, 'source_player': 3 },
 			'hand': [
-				{ 'SPADES': 1 },
-				{ 'HEARTS': 3 },
+				{ 'type': 'SPADES', 'number': 1 },
+				{ 'type', 'HEARTS', 'number': 3 },
 				# ...
 			],
 			'legal_moves': [
-				{ 'SPADES': 1 },
+				{ 'type': 'SPADES', 'number': 1 },
+				# ...
+			],
+			'current_set': [
+				{ 'type': 'CLUBS', 'number': 2, 'source_player': 2 },
+				{ 'type': 'CLUBS', 'number': 1, 'source_player': 3 },
 				# ...
 			],
 			# ...
